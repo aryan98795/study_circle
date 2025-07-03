66c7e6b789b520385595495cbf662c53dbdd744d
Aryan bajpai <117530164+aryan98795@users.noreply.github.com>
2025-07-03 19:45:32 +0530

    Add files via upload

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Components/Home'
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+    </Routes>
+
+  )
+}
+
+export default App
