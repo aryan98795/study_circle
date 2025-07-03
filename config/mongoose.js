5ce933bf96d8e9871c8002a1762659f8d7bb207f
Aryan bajpai <117530164+aryan98795@users.noreply.github.com>
2025-07-03 19:38:56 +0530

    Add files via upload

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/learning-platform', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error(err);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
