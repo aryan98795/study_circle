086b56a646d3972064d4c6880786c83b5eca46d0
Aryan bajpai <117530164+aryan98795@users.noreply.github.com>
2025-08-01 12:42:36 +0530

    Delete routes directory

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getUser, getUserById } = require('../controllers/userController');
-const router = express.Router();
-
-router.get('/profile/:userId',getUserById);
-router.get('/profile', )
-
-module.exports = router
