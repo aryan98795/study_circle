ab17fdf70b7f2b42a9a96731c62ad0c970af2fe5
Aryan bajpai <117530164+aryan98795@users.noreply.github.com>
2025-08-01 13:55:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ dotenv.config({
     path: './.env'
 })
 
-
-
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
@@ -17,35 +15,3 @@ connectDB()
 .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
 })
-
-
-
-
-
-
-
-
-
-
-/*
-import express from "express"
-const app = express()
-( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("errror", (error) => {
-            console.log("ERRR: ", error);
-            throw error
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
-
-    } catch (error) {
-        console.error("ERROR: ", error)
-        throw err
-    }
-})()
-
-*/
